ccd2716230655b58e2edf1a0249d514bbe68a839
Tahir Ahmad <609indo@gmail.com>
2026-09-14 22:46:55 +0200

    Add education view, route and template

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Education, Experience
 
 
 def show_main(request):
@@ -25,3 +25,11 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+
+def show_education(request):
+    context = {
+        "name": "Tahir Ahmad",
+        "education_list": Education.objects.all(),
+    }
+    return render(request, "education.html", context)
